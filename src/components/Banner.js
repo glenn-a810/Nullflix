9ca190b4438587bfd18e87d1dd1d0a20721c6822
Glenn <a810.glenn@gmail.com>
2022-09-01 21:18:05 +0900

    ive 커여워어어

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import requests from '../api/requests'
 import instance from '../api/axios'
 import './Banner.css'
+import styled from 'styled-components'
 
 function Banner() {
   const [movie, setMovie] = useState([])
+  const [isClicked, setIsClicked] = useState(false)
 
   useEffect(() => {
     fetchData()
@@ -33,29 +35,85 @@ function Banner() {
   //   console.log(movie.overview)
   // }
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
-        backgroundPosition: 'top center',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie.title || movie.name || movie.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button info">More Information</button>
+  if (!isClicked) {
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
+          backgroundPosition: 'top center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {movie.title || movie.name || movie.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => {
+                setIsClicked(true)
+              }}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+          {/*<h1 className="banner__description">{truncate(movie.overview, 100)}</h1>*/}
+          <h1 className="banner__description">{movie.overview}</h1>
         </div>
-        {/*<h1 className="banner__description">{truncate(movie.overview, 100)}</h1>*/}
-        <h1 className="banner__description">{movie.overview}</h1>
-      </div>
-      <div className="banner--fadeBottom" />
-    </header>
-  )
+        <div className="banner--fadeBottom" />
+      </header>
+    )
+  } else {
+    return (
+      <Container>
+        <HomeContainer>
+          <Iframe
+            width="640"
+            height="360"
+            src="https://www.youtube.com/embed/F0B7HDiY-10"
+            // src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?constrols=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            title="IVE 아이브 'After LIKE' MV"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          ></Iframe>
+        </HomeContainer>
+      </Container>
+    )
+  }
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
 
 export default Banner
