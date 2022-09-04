e7a6f5c201cb908488cfc4901ada1f0776ceb62b
Glenn <a810.glenn@gmail.com>
2022-09-04 21:38:36 +0900

    slide 기능

--- src/components/Row.js
+++ src/components/Row.js
@@ -19,7 +19,14 @@ function Row({ isLargeRow, title, id, fetchURL }) {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{'<'}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80
+            }}
+          >
+            {'<'}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movie) => (
@@ -34,7 +41,14 @@ function Row({ isLargeRow, title, id, fetchURL }) {
           ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{'>'}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth + 80
+            }}
+          >
+            {'>'}
+          </span>
         </div>
       </div>
     </section>
