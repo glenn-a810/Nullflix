9eafc7725f92f6ca8fce9ef256d958d99cea005f
Glenn <a810.glenn@gmail.com>
2022-09-18 20:35:56 +0900

    Updates

---
+++ precache-manifest.b949703d68e13c50b733cb7e87da77de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a81d19cb1c44170a79b9",
+    "url": "/Nullflix/static/css/main.6fd38214.chunk.css"
+  },
+  {
+    "revision": "a81d19cb1c44170a79b9",
+    "url": "/Nullflix/static/js/main.6be00258.chunk.js"
+  },
+  {
+    "revision": "5ba1f7013184b8317286",
+    "url": "/Nullflix/static/js/runtime~main.81316cb7.js"
+  },
+  {
+    "revision": "6237b5b7dbe0c06042c7",
+    "url": "/Nullflix/static/css/2.7c6b0289.chunk.css"
+  },
+  {
+    "revision": "6237b5b7dbe0c06042c7",
+    "url": "/Nullflix/static/js/2.789460a5.chunk.js"
+  },
+  {
+    "revision": "ae9de2e797326c8436a7",
+    "url": "/Nullflix/static/js/3.5d3ae9cd.chunk.js"
+  },
+  {
+    "revision": "bdedc055c965e65b2a4ba5990fd9857c",
+    "url": "/Nullflix/index.html"
+  }
+];
