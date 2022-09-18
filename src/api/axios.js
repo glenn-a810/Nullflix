6c77fc880114e55b06e95d8d26e7784d4a80af7e
Glenn <a810.glenn@gmail.com>
2022-09-18 20:53:27 +0900

    github pages

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: 'd2e2f91edbc573ffb4c025a7a22d3db7',
+    api_key: process.env.REACT_APP_API_KEY,
     language: 'ko-KR',
   },
 })
