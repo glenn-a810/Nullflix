680d8013005225b6f00f634efb20a6c3485c3ca8
Glenn <a810.glenn@gmail.com>
2022-09-19 21:21:05 +0900

    react test library - jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  // render(<App />) // DOM에 컴포넌트를 랜더링하는 함수
+  const { getByText } = render(<App />)
+  // const linkElement = screen.getByText(/learn react/i);
+  const linkElement = getByText(/NETFLIX ORIGINALS/i)
+  expect(linkElement).toBeInTheDocument()
+})
